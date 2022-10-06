93aebd78e4f4cfd42fc82ff8110e0e798b36c7bc
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-10-06 12:24:38 +0200

    refactoring server.js with express app

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,11 @@
 // creation du serveur node.js sans express avec le module  de base "http" de node js
-const http = require("http") ;// indiquer plutot https pour le certificat ssl
+const http = require("http") ;// indiquer plutot https pour le certificat ssl, sans chemin relatif car c est un module de base de node
+const app= require("./app")// indique le chemin relatif car app.js  n est pas un module de base de Node
 
-const server= http.createServer((req,res)=>{// methode "createserver"de l objet http avec fonction call back qui prend en parametre requete et reponse
-    res.end("voilà la réponse du serveur");
-});
+app.set("port",process.env.PORT || 3000);// installe le parametre port dans l application express dans app
+const server= http.createServer(app);// appel des methodes et fonctions du module app.js dans le server crée
 
 // PORT variable d environnement  avec la valeur du port  par defaut selon le systeme d exploitation de l appareil
 // soit le serveur ecoute sur le port par defaut de l appareil ou sur le port 3000
 server.listen(process.env.PORT || 3000); 
+// attend les requete et ecoute le port avec l application app express  avec le parametre port installé dans app et qui traitera les requetes et reponse dans le module app.js
