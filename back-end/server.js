af414bbfc7d09aa2718961144c122aae90114e37
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-10-06 23:24:11 +0200

    refactoring srever.js listener on() nomalize port, errorhandkers

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,64 @@
-// creation du serveur node.js sans express avec le module  de base "http" de node js
-const http = require("http") ;// indiquer plutot https pour le certificat ssl, sans chemin relatif car c est un module de base de node
-const app= require("./app")// indique le chemin relatif car app.js  n est pas un module de base de Node
+const { NOMEM } = require('dns');
+const http = require('http');
+const app = require('./app');
 
-app.set("port",process.env.PORT || 3000);// installe le parametre port dans l application express dans app
-const server= http.createServer(app);// appel des methodes et fonctions du module app.js dans le server crée
+const normalizePort = val => {
+  const port = parseInt(val, 10);
 
-// PORT variable d environnement  avec la valeur du port  par defaut selon le systeme d exploitation de l appareil
-// soit le serveur ecoute sur le port par defaut de l appareil ou sur le port 3000
-server.listen(process.env.PORT || 3000); 
-// attend les requete et ecoute le port avec l application app express  avec le parametre port installé dans app et qui traitera les requetes et reponse dans le module app.js
+  if (isNaN(port)) { // si cest port est  string, renvoit le port de env ou 3000
+    return val;
+  }
+  if (port >= 0) {// si port est un nombre  renvoyé par parseInt, renvoit le resultat de parseInt
+    return port;
+  }
+  return false;// si port ne rentre pas dans la condition isNan (si c est un string) et ne rentre pas dans la condition d etre un nombre, la fonction normaliseport retourne false dans la variable port ci dessous
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {// les erreurs de sytems ont des nom, systemcall renvoit une chaine de caractere avec le nom de l erreur de l appel du system
+    throw error; //stoppe le programme et fournit la valeur de l exception  stocker ds "error" 
+  }
+  const address = server.address();
+  /*  Lorsqu'une socket est créée avec socket(point de terminaison) , elle existe sous un nom
+       espace (famille d'adresses) mais n'a pas d'adresse qui lui soit assignée.  bind ()
+       attribue l'adresse spécifiée par addr à la socket référencée
+       par le descripteur de fichier sockfd .  addrlen spécifie la taille, en
+       octets, de la structure d'adresse pointée par addr .
+       Traditionnellement, cette opération s'appelle « attribuer un nom à un
+       prise"*/
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port; 
+  switch (error.code) {
+    // erreur.code:prorieté code renvoit une chaine avec le code de l erreur du systeme couramment rencontré lors de l ecriture d un programme Node
+    //, switch verifie la valeur retourné de code avec diffrent code erreur ci dessous
+    case 'EACCES':// Autorisation refusée) :
+ // une tentative d'accès à un fichier d'une manière interdite par ses autorisations d'accès au fichier a été effectuée.
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1); //le code 1 force l echec du process, le code 0 : code succes par defaut 
+    //  mieux vaut definir en amont avec process.exitCode= "le code", le code de sortie du process a definir lorsque le process node se termine normalement, node peut au moins terminer sa boucle d evenement sans forcer l/ echec
+      break;
+
+    case 'EADDRINUSE'://(Adresse déjà utilisée) :
+    // Une tentative de liaison d'un serveur ( net, http, ou https) à une adresse locale a échoué car un autre serveur sur le système local occupait déjà cette adresse.
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+      
+   /*  default definit le block de code par defaut qui sera execute 
+   si l instruction switch ne trouve auucne correspondance de valeurs(dans les case) avec la valeur de error.code*/
+    default: 
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler); //0n() est un ecouteeur d evenemnt, ici l ecouteur d evenment ecoute les evenement erreur qui se produit sur le server)
+server.on('listening', () => { // ecoute les evenement nomme listening qui se produit sur server
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
