3c6ee578b2eaee957bea0f6221fce62fac1c7668
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-10-07 14:00:13 +0200

    configuration response data modify security CORS

--- back-end/app.js
+++ back-end/app.js
@@ -2,10 +2,19 @@ const express = require("express");
 
 const app= express();
 
+// configuration generale de la reponse retourné suite à une requete du front end avec la modification de la securite d acces au ressource de CORS en ajoutant des headers à l objet response pour tout type de requete et sur toutes les routes
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  // apres cette configuration l acces au donnes de stuff sur l endpoints api/stuff sera accessible depuis different serveur ou origine pour tout type de requete http
 
 // la fonction use()  ajoute le gestionnaire de route ou un middleware au niveau de l application ou du router et ici  ajoute a la pile des middleware au noveau de l application
 // use ajoute une root et un middleware sur toutes types de requete http , le middleware envoit sur tt type de requete le contenu des articles stuff
-app.use('/api/stuff', (req, res, next) => {
+app.use('/api/stuff', (req, res) => {
     const stuff = [
       {
         _id: 'oeihfzeoi',
