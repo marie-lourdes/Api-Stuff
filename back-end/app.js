4693ced50f36cf7b7062ad1cd8af80d71680e4cb
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-10-07 12:51:37 +0200

    create data api in endpoint api/stuff

--- back-end/app.js
+++ back-end/app.js
@@ -2,23 +2,30 @@ const express = require("express");
 
 const app= express();
 
-// le gestionnaire de route (req, res){]traite les requetes  de tous les verbes http et  sur toutes les route avec la fonction use() et sans la fonction next()
+
 // la fonction use()  ajoute le gestionnaire de route ou un middleware au niveau de l application ou du router et ici  ajoute a la pile des middleware au noveau de l application
-app.use((req,res,next)=>{
-    console.log("requête reçue")
-   next(); //passe l execution du serveur au au middleware suivant en appelant la fonction suivant avec la fonction de rappel next()
-});
-app.use((req, res,next)=>{
-    res.status(201); //ce middleware modifie le status de l'objet response
-    next();
-})
-app.use((req, res,next)=>{
-    res.json({message: "message: Votre requete a bien été recue"})
-    next();
-})
-app.use((req, res)=>{// l execition du serveur pour la requete est terminé pas besoin de next() et des middleware ici, c'est gestionnaire de route et plus une fonction middleware car n a pas de next() en troisieme argument et il n est plus necessaire de traiter la requête  
-    console.log("la reponse a bien été envoyé")
-})
+// use ajoute une root et un middleware sur toutes types de requete http , le middleware envoit sur tt type de requete le contenu des articles stuff
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff); //le middleware modifie le status de la reponse et envoie la reponse avec l'objet stuff au format json en resultat de sortie avec la fonction json()
+  });
 
 /* module.exports exporte et rend accessible  la valeur actuelle de l'objet exports(du module app.js) 
 qui sont les fonctions et methodes de l'application express contenu  dans la variable  const app */
