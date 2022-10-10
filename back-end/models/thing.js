d20ba50f886a37ff3ada2343c703da668a4a2c04
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-10-10 15:06:59 +0200

    thing.js: create shema and model exports

---
+++ back-end/models/thing.js
@@ -0,0 +1,16 @@
+const mongoose = require( "mongoose" );
+
+
+// creation du shema a exporter dans un model
+//on ajoute pas le champs id car l id est generé automatique par la base de données mongoDB
+
+const thingSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+// On crée un model qu on exporte avec la valeur du shema
+module.exports = mongoose.model('Thing', thingSchema);
