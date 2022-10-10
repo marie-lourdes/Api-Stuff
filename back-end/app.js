fc67b498eebc8772ec42894c96b64d5976bddccf
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-10-10 16:50:10 +0200

    app.js: import model thing and save thing route POST  with methode save() de mongoose

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose= require("mongoose");
+const Thing = require("./models/thing"); // import du module thing.js contenant la valeur actuelle du model avec  la valeur de thingshema 
 
 const app= express();
 mongoose.connect('mongodb+srv://marie-lourdes:1234@cluster0.bq9wlht.mongodb.net/?retryWrites=true&w=majority',
@@ -24,9 +25,18 @@ app.use((req, res, next) => {
   // apres cette configuration l acces au donnes de stuff sur l endpoints api/stuff sera accessible depuis different serveur ou origine pour le verbe GET http
   // la methode post() ajoute la route et le middleware qui traitera la requete poste et l objet response de la requete post
   app.post('/api/stuff', (req, res, next) => {
-    console.log( "objet crée",req.body);// capture du contenu de la requete post dans la console
-    res.status(201).json({// pour que la requete post aboutisse,on envoit la reponse et modifier le code de succes en 201 qui corresponde à une demande de post reussi
-      message: 'Objet créé !'
+    app.post('/api/stuff', (req, res, next) => {
+      delete req.body._id;// on efface au preable l id generé par le front-end car la base de données mongo genere deja l id
+      const thing = new Thing({// on crée une instance  du model Thing stocké dans la variable thing
+        ...req.body// "spread ... " operateur js qui copie et colle tous les elements  du body de la requete post utilisateur dans l'instance du model thing
+      });
+      // on enregistre le thing dans la base de données  avec la methode save() de mongoose
+      thing.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+        // save() envoie un promesse, sur ce resultat de la promesse denregistrement de l 'objet  avec then():
+        //on envoie la reponse avec le status 201 et le msg en json
+        //on recupere l erreur et on envoie le status erreur 400 avec la reponse de la requete et le message d erreur de catch
+        .catch(error => res.status(400).json({ error })); // est le raccourci de.json( error:error):error module separé  de l objet error
     });
   });
 
--- back-end/models/thing.js
+++ back-end/models/thing.js
@@ -2,15 +2,15 @@ const mongoose = require( "mongoose" );
 
 
 // creation du shema a exporter dans un model
-//on ajoute pas le champs id car l id est generé automatique par la base de données mongoDB
+//on ajoute pas le champs id car l id est generé automatiquement par la base de données mongoDB
 
 const thingSchema = mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   imageUrl: { type: String, required: true },
   userId: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: { type: Number, required: true }, // objet de configuration des données, valeur des propriétés  de l objet thingShema
 });
 
-// On crée un model qu on exporte avec la valeur du shema
+// On crée un model qu on exporte avec la valeur de l objet shema
 module.exports = mongoose.model('Thing', thingSchema);
