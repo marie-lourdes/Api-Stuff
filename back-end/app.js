87ad0fb13dd8aea0fa2073087d0b27f3e8c2097a
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-10-11 12:42:25 +0200

    app.js: route get thing model

--- back-end/app.js
+++ back-end/app.js
@@ -31,6 +31,7 @@ app.use((req, res, next) => {
       // "spread ... " operateur js qui copie et colle tous les elements  du body de la requete post utilisateur dans l'instance du model thing
     // au lieu de title: req.body.title;
     //description: req.body.description
+   
     });
     // on enregistre le thing dans la base de données  avec la methode save() de mongoose
     thing.save()
@@ -40,32 +41,20 @@ app.use((req, res, next) => {
       //on recupere l erreur et on envoie le status erreur 400 avec la reponse de la requete et le message d erreur de catch en objet json
       .catch(error => res.status(400).json({ error })); // est le raccourci de.json( {error:error}):error module separé  de l objet error
   });
-    
+
 // la methode  get()  ajoute le middleware au niveau de l application  et ajoute la route sur toute les requete avec le verbe HTTP GET
 // le middleware ajouté a la methode get traitera les requete http GET, ici traite l objet response
-app.get('/api/stuff', (req, res) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff); //le middleware modifie le status de la reponse et envoie la reponse avec l'objet stuff au format json en resultat de sortie avec la fonction json()
+// récuperation de la liste de things dans le modele Thing
+
+  app.get('/api/stuff', (req, res, next) => {
+    Thing.find()// recupere tous les elements du modele Thing
+      .then(things => res.status(200).json(things)) // then evite les call back a l interieur de la fonction find, execute un instruction sur la promesse retourné par find()
+      .catch(error => res.status(400).json({ error }));
   });
 
 
 /* module.exports exporte et rend accessible  la valeur actuelle de l'objet exports(du module app.js) 
 qui sont les fonctions et methodes de l'application express contenu  dans la variable  const app */
 module.exports= app;
+
+
