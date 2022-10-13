4e36ea7ecde5f8821c7cf48a845b858ddfb00018
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-10-13 19:16:51 +0200

    controllers Users.js: add function controllers login authentification connexion width findOne email user width bcrypt compare() passeword

--- back-end/controllers/Users.js
+++ back-end/controllers/Users.js
@@ -3,6 +3,7 @@ const bcrypt= require("bcrypt");
 //import du model User qui sera enregistré dans la base de données avec les fonctions signup et login
 const User= require("../models/Users");
 
+//............... fonction controllers sign up pour l inscription/la creation d un compte utilisateur.....................
 exports.signup = (req, res, next) => {
     //on demande a l 'algorithme de bcrypt de hasher et anlyser 10 fois le mot de passe
     bcrypt.hash(req.body.password, 10)// on crypte le mot de passe que l utilisateur entre lors de la requete post via le formulaire d inscription
@@ -22,6 +23,28 @@ exports.signup = (req, res, next) => {
 
 };
 
+//............... fonction controllers login pour la verification de connexion  utilisateur (identifiants email et mode passe).....................
 exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+    .then(user => { //then recupere et verifie le resultat de la promesse renvyé par la methode findOne()
+        if (!user) { //si l email à retrouver n existe pas ,   n est pas retrouvé dans la base de données, absente, donc false !user dans le resultat de  la promesse de findOne 
+            return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});// le then envoit une reponse indiquant l erreur 401 acces non autorisé
+        }
+// la methode compare() verifie dans le resulat " user" recuperé par then:
+// ( utilisateur trouvé dans la base de donné via la requete de comparaison de l email de findOne() ) la correspondance du mot de passe de l utilisateu trouvé dans la base de donnée 
+// avec le mot de passe entrée dans le formulaire de connexion via la requete POST de l utilisateur
+        bcrypt.compare(req.body.password, user.password)//renvoit une valeur booléenne
+            .then(valid => {
+                if (!valid) {
+                    return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' }); //si l email existe mais le mot de passe incorrecte, erreur 401 acces non autorise
+                }
+                res.status(200).json({
+                    userId: user._id,
+                    token: 'TOKEN'
+                });
+            })
+            .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 
 };
